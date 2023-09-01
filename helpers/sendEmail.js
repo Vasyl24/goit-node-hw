a184fefc264af0046e3538800c713d81c2efaa41
Vasyl Shaposhnyk <aries240302@gmail.com>
2023-09-01 11:45:51 +0300

    Move email adress to .env file

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,12 +1,12 @@
 const sgMail = require('@sendgrid/mail');
 require('dotenv').config();
 
-const { SENDGRID_API_KEY } = process.env;
+const { SENDGRID_API_KEY, EMAIL } = process.env;
 
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 const sendEmail = async (data) => {
-  const email = { ...data, from: 'tlisovets@gmail.com' };
+  const email = { ...data, from: EMAIL };
   await sgMail.send(email);
   return true;
 };
